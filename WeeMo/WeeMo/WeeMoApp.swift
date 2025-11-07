ba1aeac1ebcc242fd0ffe6f8968f06e6c94b8051
Youngjin <bryjna07@nate.com>
2025-11-07 16:03:31 +0900

    Chore: WeeMo 프로젝트 생성
    Resolves: #1

---
+++ WeeMo/WeeMo/WeeMoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeeMoApp.swift
+//  WeeMo
+//
+//  Created by YoungJin on 11/7/25.
+//
+
+import SwiftUI
+
+@main
+struct WeeMoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
