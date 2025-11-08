f0c3b4b77647b948c4aa4622e8e9201cd2d7a1aa
우다당탕 개발자 <uddt_ds@kakao.com>
2025-11-08 23:28:08 +0900

    Feat: 회원가입 UI 구현 #7
    - 회원가입 UI 구현
    - UI 구현에 따른 LoginView NavigationView 연결

--- WeeMo/WeeMo/Auth/LoginView.swift
+++ WeeMo/WeeMo/Auth/LoginView.swift
@@ -10,106 +10,101 @@ import SwiftUI
 struct LoginView: View {
     @State private var id = ""
     var body: some View {
-        ZStack {
-            Color.wmBg
-                .ignoresSafeArea()
-
-            VStack(spacing: 10) {
-                VStack(spacing: 6) {
-                    /// 폰트, 컬러 사용 예시
-                    Text("WeeMo")
-                        .font(.app(.headline1))
-                        .foregroundStyle(.wmMain)
-                    Text("공간을 예약하고 모임을 만들어 보세요")
-                        .font(.app(.content2))
-                        .foregroundStyle(.textSub)
-                }
-
-                VStack(alignment: .leading) {
-                    VStack(alignment: .leading, spacing: 12) {
-                        VStack(alignment: .leading, spacing: 4) {
-                            Text("아이디")
-                                .font(.app(.content1))
-                            TextField("아이디를 입력하세요", text: $id)
-                                .frame(height: 12)
-                                .asGrayBackgroundView()
-                            Text("이메일 형식이 잘못되었어요")
-                                .font(.app(.subContent3))
-                                .foregroundStyle(.red)
-                        }
-
-                        VStack(alignment: .leading, spacing: 4) {
-                            Text("비밀번호")
-                                .font(.app(.content1))
-                            TextField("비밀번호를 입력하세요", text: $id)
-                                .frame(height: 12)
-                                .asGrayBackgroundView()
-                            Text("비밀번호 형식이 잘못되었어요")
-                                .font(.app(.subContent3))
-                                .foregroundStyle(.red)
-                        }
-
-                        Button {
-                            print("버튼 클릭")
-                        } label: {
-                            Text("로그인")
-                                .font(.app(.subHeadline2))
-                                .foregroundStyle(.white)
-                                .frame(maxWidth: .infinity,
-                                       maxHeight: 46,
-                                       alignment: .center)
-                                .background(.wmMain)
-                                .cornerRadius(8)
-                        }
-
-                        ZStack {
-                            Rectangle()
-                                .frame(height: 1)
-                                .background(.textSub)
-
-                            Text("또는")
-                                .padding(10)
-                                .foregroundStyle(.textSub)
-                                .background(.white)
-                        }
-
-                        Button {
-                            print("버튼 클릭")
-                        } label: {
-                            Image("kakao_login_large_wide")
-                                .resizable()
-                                .frame(width: .infinity, height: 46)
-                        }
-
-                        Button {
-                            print("버튼 클릭")
-                        } label: {
-                            Text("Apple 로그인")
-                                .bold()
-                                .foregroundStyle(.white)
-                                .padding(20)
-                                .frame(maxWidth: .infinity,
-                                       maxHeight: 46,
-                                       alignment: .center)
-                                .background(.black)
-                                .cornerRadius(8)
-                        }
-
-                        Button {
-                            print("버튼 클릭")
-                        } label: {
-                            Text("이메일로 회원가입")
-                                .font(.app(.content2))
-                        }
-                        .frame(maxWidth: .infinity, alignment: .center)
+        NavigationView {
+            ZStack {
+                Color.wmBg
+                    .ignoresSafeArea()
+                
+                VStack(spacing: 10) {
+                    VStack(spacing: 6) {
+                        /// 폰트, 컬러 사용 예시
+                        Text("WeeMo")
+                            .font(.app(.headline1))
+                            .foregroundStyle(.wmMain)
+                        Text("모두를 위한 모임, 위모")
+                            .font(.app(.content2))
+                            .foregroundStyle(.textSub)
                     }
-                    .padding(30)
-
+                    
+                    VStack(alignment: .leading) {
+                        VStack(alignment: .leading, spacing: 12) {
+                            VStack(alignment: .leading, spacing: 4) {
+                                Text("아이디")
+                                    .font(.app(.content2))
+                                TextField("아이디를 입력하세요", text: $id)
+                                    .frame(height: 12)
+                                    .asMintCornerView()
+                                Text("이메일 형식이 잘못되었어요")
+                                    .font(.app(.subContent2))
+                                    .foregroundStyle(.red)
+                            }
+                            
+                            VStack(alignment: .leading, spacing: 4) {
+                                Text("비밀번호")
+                                    .font(.app(.content2))
+                                TextField("비밀번호를 입력하세요", text: $id)
+                                    .frame(height: 12)
+                                    .asMintCornerView()
+                                Text("비밀번호 형식이 잘못되었어요")
+                                    .font(.app(.subContent2))
+                                    .foregroundStyle(.red)
+                            }
+                            
+                            Button {
+                                print("버튼 클릭")
+                            } label: {
+                                Text("로그인")
+                                    .font(.app(.subHeadline2))
+                                    .foregroundStyle(.white)
+                                    .frame(maxWidth: .infinity,
+                                           maxHeight: 46,
+                                           alignment: .center)
+                                    .background(.wmMain)
+                                    .cornerRadius(8)
+                            }
+                            
+                            ZStack {
+                                Rectangle()
+                                    .frame(height: 1)
+                                    .background(.textSub)
+                                
+                                Text("또는")
+                                    .padding(10)
+                                    .foregroundStyle(.textSub)
+                                    .background(.white)
+                            }
+                            
+                            Button {
+                                print("버튼 클릭")
+                            } label: {
+                                Image("kakao_login_large_wide")
+                                    .resizable()
+                                    .frame(width: .infinity, height: 46)
+                            }
+                            
+                            Button {
+                                print("버튼 클릭")
+                            } label: {
+                                AppleLoginButtonUI()
+                            }
+                            
+                            NavigationLink {
+                                SignView()
+                            } label: {
+                                Text("이메일로 회원가입")
+                                    .font(.app(.content2))
+                            }
+                            .frame(maxWidth: .infinity, alignment: .center)
+                        }
+                        .navigationTitle("")
+                        .padding(30)
+                        
+                    }
+                    .background(.white)
+                    .cornerRadius(16)
+                    .padding(20)
+                    .shadow(radius: 3)
                 }
-                .background(.white)
-                .cornerRadius(16)
-                .padding(20)
-                .shadow(radius: 3)
             }
         }
     }
---
+++ WeeMo/WeeMo/Auth/SignView.swift
@@ -0,0 +1,90 @@
+//
+//  SignView.swift
+//  WeeMo
+//
+//  Created by Lee on 11/8/25.
+//
+
+import SwiftUI
+
+struct SignView: View {
+    @State private var id: String = ""
+    var body: some View {
+        ZStack {
+            Color.wmBg
+                .ignoresSafeArea()
+
+            VStack(spacing: 6) {
+                VStack {
+                    Text("회원가입")
+                        .font(.app(.headline3))
+                    
+                    Rectangle()
+                        .frame(width: 20, height: 2)
+                        .foregroundStyle(.wmMain)
+                }
+
+                VStack(spacing: 10) {
+                    VStack(alignment: .leading) {
+                        Text("아이디")
+                            .font(.app(.content1))
+                        TextField("아이디를 입력하세요", text: $id)
+                            .frame(height: 12)
+                            .asMintCornerView()
+                        Text("이메일 형식이 잘못되었어요")
+                            .font(.app(.subContent3))
+                            .foregroundStyle(.red)
+                    }
+                    .padding(.horizontal, 20)
+                    .padding(.top, 20)
+
+                    VStack(alignment: .leading) {
+                        Text("비밀번호")
+                            .font(.app(.content1))
+                        TextField("비밀번호를 입력하세요", text: $id)
+                            .frame(height: 12)
+                            .asMintCornerView()
+                        Text("이메일 형식이 잘못되었어요")
+                            .font(.app(.subContent3))
+                            .foregroundStyle(.red)
+                    }
+                    .padding(.horizontal, 20)
+
+                    VStack(alignment: .leading) {
+                        Text("닉네임")
+                            .font(.app(.content1))
+                        TextField("닉네임을 입력하세요", text: $id)
+                            .frame(height: 12)
+                            .asMintCornerView()
+                        Text("닉네임 형식이 잘못되었어요")
+                            .font(.app(.subContent3))
+                            .foregroundStyle(.red)
+                    }
+                    .padding(.horizontal, 20)
+
+                    Button {
+                        print("버튼 클릭")
+                    } label: {
+                        Text("가입하기")
+                            .font(.app(.subHeadline2))
+                            .frame(maxWidth: .infinity, maxHeight: 46)
+                            .foregroundStyle(.white)
+                            .background(.wmMain)
+                    }
+                    .cornerRadius(12)
+                    .padding(20)
+                }
+                .background(.white)
+                .cornerRadius(12)
+                .padding(20)
+                .shadow(radius: 3)
+            }
+            }
+
+
+    }
+}
+
+#Preview {
+    SignView()
+}
