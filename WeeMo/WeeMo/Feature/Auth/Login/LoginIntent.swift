38189e5775e6f8e96091ca64818e3e85d540afd9
우다당탕 개발자 <uddt_ds@kakao.com>
2025-11-14 15:14:28 +0900

    Feat: MVI 구조를 위한 Intent, State 구현 #24
    - Enum Type의 LoginIntent 구현
    
    - Login State 구현

---
+++ WeeMo/WeeMo/Feature/Auth/Login/LoginIntent.swift
@@ -0,0 +1,14 @@
+//
+//  LoginIntent.swift
+//  WeeMo
+//
+//  Created by Lee on 11/14/25.
+//
+
+import Foundation
+
+enum LoginIntent {
+    case idChanged(String)
+    case pwChanged(String)
+    case loginTapped
+}
