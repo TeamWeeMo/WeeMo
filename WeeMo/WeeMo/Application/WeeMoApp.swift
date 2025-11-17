17cde76a28b82ef3d95da78b18683d70bf2b1897
우다당탕 개발자 <uddt_ds@kakao.com>
2025-11-17 16:52:34 +0900

    Feat: Kakao SDK 초기화 및 URL 처리 추가 #24
    - WeeMoApp SDK 초기화
    
    - onOpenURL로 카카오 로그인 URL 처리

--- WeeMo/WeeMo/Application/WeeMoApp.swift
+++ WeeMo/WeeMo/Application/WeeMoApp.swift
@@ -6,20 +6,38 @@
 //
 
 import SwiftUI
+import KakaoSDKCommon
+import KakaoSDKAuth
 
 @main
 struct WeeMoApp: App {
 
     @StateObject private var appState = AppState()
 
+    init() {
+        if let appKey = Bundle.main.object(forInfoDictionaryKey: "KakaoKey") as? String {
+            KakaoSDK.initSDK(appKey: appKey)
+        }
+    }
+
     var body: some Scene {
         WindowGroup {
             if appState.isLoggedIn {
                 TapView()
                     .environmentObject(appState)
+                    .onOpenURL { url in
+                        if AuthApi.isKakaoTalkLoginUrl(url) {
+                            _ = AuthController.handleOpenUrl(url: url)
+                        }
+                    }
             } else {
                 LoginView()
                     .environmentObject(appState)
+                    .onOpenURL { url in
+                        if AuthApi.isKakaoTalkLoginUrl(url) {
+                            _ = AuthController.handleOpenUrl(url: url)
+                        }
+                    }
             }
         }
     }
